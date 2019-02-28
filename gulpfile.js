adb53315f2446c8b802009f9f97ffd045d934ce2
Danny van der Jagt <dannyvanderjagt@gmail.com>
2019-02-28 16:35:39 +0100

    [V2] the start of the new website

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-const gulp = require('gulp')
+const { src, dest, series, watch } = require('gulp')
 const sass = require('gulp-sass')
 const pug = require('gulp-pug')
 const plumber = require('gulp-plumber')
 const liveReload = require('gulp-server-livereload')
+const fs = require('fs')
 
-const data = require('./src/data.js')
 
 const paths = {
   src: {
@@ -18,43 +18,44 @@ const paths = {
   assets: './src/assets/**/*.*',
 }
 
-gulp.task('styles', () =>
-  gulp
-    .src(paths.src.styles)
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(gulp.dest(paths.public.default))
-)
+function html(){
+  delete require.cache[require.resolve('./src/data.js')]
+  const data = require('./src/data.js')
 
-gulp.task('html', () =>
-  gulp
-    .src(paths.src.html)
+  return src(paths.src.html)
     .pipe(plumber())
     .pipe(pug({
       data,
     }))
-    .pipe(gulp.dest(paths.public.default))
-)
+    .pipe(dest(paths.public.default))
+}
 
-gulp.task('assets', () =>
-  gulp
-    .src(paths.assets)
-    .pipe(gulp.dest(`${paths.public.default}/assets`))
-)
+function css() {
+  return src(paths.src.styles)
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(dest(paths.public.default))
+}
 
-gulp.task('livereload', function() {
-  gulp.src(paths.public.default)
+function assets(){
+  return src(paths.assets)
+    .pipe(dest(`${paths.public.default}/assets`))
+}
+
+function livereload(){
+  return src(paths.public.default)
     .pipe(liveReload({
       livereload: true,
-      open: true
+      open: true,
     }))
-})
+}
 
+function watchForChanges(){
+  watch(paths.src.data, html)
+  watch(paths.src.styles, css)
+  watch(paths.src.html, html)
+  watch(paths.assets, assets)
+}
 
-gulp.task('data:watch', () => gulp.watch(paths.src.data, ['html']))
-gulp.task('styles:watch', () => gulp.watch(paths.src.styles, ['styles']))
-gulp.task('html:watch', () => gulp.watch(paths.src.html, ['html']))
-gulp.task('assets:watch', () => gulp.watch(paths.assets, ['assets']))
-
-gulp.task('default', ['html', 'styles', 'assets', 'watch', 'livereload'])
-gulp.task('watch', ['html:watch', 'styles:watch', 'assets:watch', 'data:watch'])
+exports.buildAndWatch = series(html, css, assets, livereload, watchForChanges)
+exports.build = series(html, css, assets);
--- src/data.js
+++ src/data.js
@@ -17,75 +17,79 @@ module.exports = {
       url: 'http://www.linkedin.com',
     },
   ],
-  title: 'Hello there!',
 
   // Sections
-  whoIAm:{
-    quote: '"An combination of determination, creativity and curiosity"',
-    content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus."
-  },
-
-  experience: {
-    quote: "“An art of teamwork, ideas and perservance.”",
-    items: [
-      {
-        featureImage: 'assets/project-background-creative-confidence.jpg',
-        jobDescription: 'Founder & Writer',
-        title: ["Creative","Confidence"],
-        quote: "”Writings about a combination of psychology, biology and technology”",
-        content: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus.",
-        buttons: [
-          { title: 'Comming soon' }
-        ]
-      },
-      {
-        featureImage: 'assets/project-background-learn-to-code.png',
-        jobDescription: 'Co-organizer',
-        title: ["Learn to","code","Together"],
-        quote: "“A meetup in Amsterdam, for everyone”",
-        content: "A meetup for beginner to professional developers. I am responsible for our logo and designs, doing mentorship, teaching and creating a curriculum for our members.",
-        buttons: [
-          { title: 'Visit' }
-        ]
-      },
-      {
-        featureImage: 'assets/project-background-wayke.jpg',
-        jobDescription: 'Lead Developer',
-        title: ["Lightbase &","Wayke"],
-        quote: "“A newly founded startup by Volkswagen and Volvo in Sweden”",
-        content: "I joined Lightbase as the first employee helping build the digital agency while growing from 1 to 7 employees in 9 months. Responsible for introducing the startup culture, project- and technical architecture. Leading the development of the React-Native app. Responsible for the Wayke app, project and technical architecture, project and team management, reaching an extremely tight deadline, developing our most complex features and creator of our local search mirror algorithm. A two-person development team, mentoring our freshly graduated developer.",
-        achivements: [
-          { title: "+ 1 Million active users"}
-        ],
-        buttons: [
-          { title: 'Showcase' },
-          { title: 'Visit', url: 'wayke.se'}
-        ]
-      },
-      {
-        featureImage: 'assets/project-background-florin.jpg',
-        jobDescription: 'Lead Developer',
-        title: ["Florin"],
-        quote: "“A fin-tech startup which partnered with the startup-studio Backspace and is financed by the ING”",
-        content: "As part of a creative multidisciplinary team, I got to work with and learn from a lot of experts in fields ranging from development, design, storytelling, brainstorming, finance, and business while experiencing and learning how to start and grow a startup. Responsible for developing our app, making performance improvements, developing new features with our backend team and introducing animations. I was in charge of exploring new product developments and designs, new features, releases and overall performance. In 2 months time, I went on to be the Lead Developer and in charge of exploring the development of new products, design, new features, releases and overall performance.",
-        achivements: [
-          { title: "+ 13.000 active users"}
-        ],
-        buttons: [
-          { title: 'Showcase' },
-        ]
-      },
-      {
-        featureImage: 'assets/project-background-ing.jpg',
-        jobDescription: 'Creative Thinker, Designer, and Prototyper',
-        title: ["ING, Florin,","and Backspace"],
-        quote: "“Rethinking how people pay everyday in europe “",
-        content: "I joined Lightbase as the first employee helping build the digital agency while growing from 1 to 7 employees in 9 months. Responsible for introducing the startup culture, project- and technical architecture. Leading the development of the React-Native app. Responsible for the Wayke app, project and technical architecture, project and team management, reaching an extremely tight deadline, developing our most complex features and creator of our local search mirror algorithm. A two-person development team, mentoring our freshly graduated developer.",
-        achivements: [
-          { title: "A vision for the ING Bank"}
-        ],
-        buttons: [],
+  projects: [
+    {
+      image: 'assets/crisp-hero.jpg',
+      role: 'Front-end & React-Native developer',
+      title: ["Crisp"],
+      intro: "“A brand new online supermarket, focused fresh food”",
+      content: [
+        "Crisp is a new fully funded online-supermarket. Crisp offers super fresh, great quality products from over 200 farmers, fishermen, growers and makers via a fast and beautiful app.",
+        "I joined quite quick after it’s was founded and before the company launched in Nov 2019. Responsible for developing our app, working closely together with our product owners, UI, UX and creative teams. In addition I mentored new developers and was part of the (business) culture team.",
+      ],
+      visit: "https://crisp.nl",
+      download: {
+        ios: "https://itunes.apple.com/us/app/crisp/id1416625210?ls=1&mt=8",
+        android: "https://play.google.com/store/apps/details?id=com.freshfoodventures.crisp",
       }
-    ]
-  }
+    },
+    {
+      image: 'assets/meetup-hero.jpg',
+      role: "Co-organizer",
+      title: ["Learning", "to code", "meetup"],
+      intro: "“A fun and exciting meetup to get together and (learn how) to code, have a beer and help each other”",
+      content: [
+        "We support the beginner and advanced developer by providing resources, sharing our knowledge and providing guidance. We provide a team of professional developers who are willing to help everybody.",
+        "As a co-organiser, I am responsible for developing our meetup, our logo & visual designs, mentorship & teaching and helping our team in the creation of a curriculum for our members.",
+      ],
+      visit: "https://www.meetup.com/Learning-to-Code-Amsterdam/"
+    },
+    {
+      image: 'assets/wayke-hero.jpg',
+      role: "Mobile Lead & React-Native developer",
+      title: ["Wayke"],
+      intro: "“A next-level car market place, a founded startup by Volkswagen and Volvo in Sweden”",
+      content: [
+        "I was the lead-developer for mobile from day one, we launched in Nov ’18 with a stable and fully-featured app.",
+        "Within in Wayke, I was responsible for the Wayke app, project and technical architecture, team management, reaching our extremely tight deadlines, focused on developing our UI and most complex features, creator of our local search mirror algorithm. A two-person development team, mentoring our freshly graduated developer, who continued to lead the project after I left.",
+      ],
+      visit: "https://wayke.se",
+      download: {
+        ios: "https://itunes.apple.com/se/app//id1270128998?mt=8",
+        android: "https://play.google.com/store/apps/details?id=se.wayke.app&hl=en_US"
+      }
+    },
+    {
+      image: "assets/lightbase-hero.jpg",
+      role: "Front-end & React-Native developer",
+      title: ["Lightbase"],
+      intro: "“A new digital agency based in Utrecht”",
+      content: [
+        "I joined Lightbase as the first employee, helping them build the digital agency while we grow from 1 to 7 employees in 9 months. Responsible for introducing the startup culture, project- and technical architecture and mentoring our new developers.",
+      ],
+      visit: "https://lightbase.nl"
+    },
+    {
+      image: "assets/florin-hero.jpg",
+      role: "Product & React-Native Developer",
+      title: ["Florin"],
+      intro: "“A fin-tech startup offering payment request to friends (before it was widly adopted). Partnered with the startup-studio Backspace and financed by the ING”",
+      content: [
+        "As part of a creative multidisciplinary team, I got to work with and learn from a lot of experts in fields ranging from development, design, storytelling, brainstorming, finance, and business while experiencing and learning how to start and grow a startup. ",
+        "Responsible for developing our app, making performance improvements, developing new features with our backend team and introducing animations. I was in charge of exploring new product developments and designs, new features, releases and overall performance. In 2 months time, I went on to be the lead developer and in charge of our overall mobile development.",
+      ]
+    },
+    {
+      image: "assets/ing-hero.jpg",
+      role: "Creative Thinker & Prototyper",
+      intro: "“Completly reinventing the personal payment space in Europe“",
+      title: ["ING", "Innovation", "Session"],
+      content: [
+        "A 3-month long project, joined by Florin, Backspace, and the ING, with the mission to envision a new state of Europe's personal payment space. We got the opportunity the rethink how people pay in the future, to friends, companies and the government while making use of new technologies, laws, and culture.",
+        "While joining brainstorm sessions I contributed by providing out-of-the box perspectives, combining ideas, performing research, sharing knowledge of technology & design. In the user test stage I was responsible for designing  and developing 2 prototypes for user testing.",
+      ]
+    }
+  ]
 }
